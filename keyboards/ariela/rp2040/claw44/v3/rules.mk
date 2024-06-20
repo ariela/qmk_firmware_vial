44bedb1496c6afab424472a436462701f5e1320c
Yuki Kisaragi <ariela.elixia@gmail.com>
2024-06-20 18:45:02 +0900

    RP2040 + Vial対応

---
+++ keyboards/ariela/rp2040/claw44/v3/rules.mk
@@ -0,0 +1,20 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
+# RP2040用にシリアルドライバーを指定
+SERIAL_DRIVER = vendor
+
+#BOARD = GENERIC_RP_RP2040
+
+# リンク時最適化を行う
+LTO_ENABLE = true
